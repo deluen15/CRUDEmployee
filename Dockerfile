c8676367d5ae4a870faf3554cc9dd71c5731313f
Endrit Zeqo <endrit.zeqo@19bytes.de>
2023-07-13 12:19:02 +0200

    update workflow

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM adoptopenjdk/openjdk11:jdk-11.0.11_9-alpine-slim
+FROM eclipse-temurin:17_35-jdk-alpine
 VOLUME /main-app
-ADD target/employer-docker.jar app.jar
+ADD target/*.jar app.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/employer/repository/EmployerRepository.java
+++ src/main/java/com/example/employer/repository/EmployerRepository.java
@@ -4,9 +4,6 @@ import com.example.employer.model.Employer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface EmployerRepository extends MongoRepository<Employer, String> {
-    Optional<Employer> findEmployerByEmail(String email);
 }
