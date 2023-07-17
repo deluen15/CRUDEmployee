744b25976e888e9dab474f0a48c30f82e260cd8e
Endrit Zeqo <endrit.zeqo@19bytes.de>
2023-07-17 14:15:14 +0200

    Added zookeeper and kafka.

---
+++ src/main/java/com/example/employer/streams/KafkaStreamsHandler.java
@@ -0,0 +1,10 @@
+package com.example.employer.streams;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaStreamsHandler {
+
+}
